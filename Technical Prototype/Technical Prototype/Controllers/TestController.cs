f8dd426ff095c2f73caead4c5c0837f47e30a245
brandomjacob <brandomjacob@gmail.com>
2018-02-27 18:25:27 -0600

    Technical Prototype

---
+++ Technical Prototype/Technical Prototype/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Technical_Prototype.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /Test/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
