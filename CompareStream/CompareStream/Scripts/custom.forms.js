4be4500ef7ba830e118139ca6ce3e523387d86ae
joelbrandom <afro.hj@gmail.com>
2018-03-17 19:30:15 -0500

    jquery form submit for addShowForm

--- CompareStream/CompareStream/Scripts/custom.forms.js
+++ CompareStream/CompareStream/Scripts/custom.forms.js
@@ -14,3 +14,18 @@
         $( "#result" ).empty().append( content );
     });
 });
+
+$( "#addShowForm" ).submit(function( event ) {
+    event.preventDefault();
+
+    var $form = $( this ),
+        name = $form.find( "input[name='showName']" ).val(),
+        url = $form.attr( "action" );
+
+    var posting = $.post( url, { showName: name } );
+
+    posting.done(function( data ) {
+        var content = $( data ).filter( "#content" );
+        $( "#result" ).empty().append( content );
+    });
+});
