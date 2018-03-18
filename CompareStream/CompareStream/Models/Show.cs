1f68da1dc296f276f07f3a364603333aeff2b3e0
joelbrandom <afro.hj@gmail.com>
2018-03-17 23:20:08 -0500

    Add Shows model

---
+++ CompareStream/CompareStream/Models/Show.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CompareStream.Models
+{
+    public class Show
+    {
+        private string _name;
+
+        public Show(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+    }
+}
