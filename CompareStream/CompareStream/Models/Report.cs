d9cb67cd515649531e4bfbedc8f93b1f19f0ad5d
joelbrandom <afro.hj@gmail.com>
2018-04-23 21:50:40 -0500

    Update Report class to include userID and isFixed

--- CompareStream/CompareStream/Models/Report.cs
+++ CompareStream/CompareStream/Models/Report.cs
@@ -4,9 +4,10 @@ namespace CompareStream.Models
     public class Report
     {
         private string _description;
-        private int _id;
+        private int _id, _uid;
+        bool _fixed;
 
-        public Report(int id, string description)
+        public Report(int id, int userID, string description, bool isFixed)
         {
             ID = id;
             Description = description;
@@ -22,6 +23,18 @@ namespace CompareStream.Models
         {
             get { return _id; }
             set { _id = value; }
+        }
+
+        public int UID
+        {
+            get { return _uid; }
+            set { _uid = value; }
+        }
+
+        public bool Fixed
+        {
+            get { return _fixed; }
+            set { _fixed = value; }
         }
     }
 }
