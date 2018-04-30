504eb2941c9a0cc41400a529219168fc205ddab0
joelbrandom <afro.hj@gmail.com>
2018-04-29 22:45:24 -0500

    Update Network model

--- CompareStream/CompareStream/Models/Network.cs
+++ CompareStream/CompareStream/Models/Network.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 namespace CompareStream.Models
 {
-    public class EmptyClass
+    public class Network
     {
-        public EmptyClass()
+        private string _name;
+        private int _id;
+
+        public Network(int id, string name)
         {
+            ID = id;
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
         }
     }
 }
